9f322bfe99728c0e1967e703fc762fba1719972c
caina150909 <cainadpchaves@gmail.com>
2024-10-27 13:21:28 -0300

    tipos de dados

---
+++ tipos_de_dados.js
@@ -0,0 +1,19 @@
+// tipos de dados: 
+
+console.log("caneta"); // dados em string 
+
+console.log(10); // dados em valor inteiro - literal
+
+console.log(6.5); // dados em valor float - quebrado
+
+// variavel: cria um espaço de memoria para armazenar algo
+var quantidade = 20;
+var preco = 6.4;
+var imposto = 1.5;
+var total = preco + imposto;
+// Chamando as variaveis:
+console.log("CANETA")
+console.log("a quantidade é: ",quantidade)
+console.log("o preço é: ",preco)
+console.log("O imposto é: ", imposto)
+console.log("TOTAL A PAGAR:", "$",total,"reais")
