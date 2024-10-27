388f90e1fca16c82b42bcb13579a5ee2b6babd0e
caina <165224583+caina150909@users.noreply.github.com>
2024-10-27 14:24:03 -0300

    Update calculadora_nivel1.js

---
+++ calculadora_nivel1.js
@@ -0,0 +1,12 @@
+let a = 4
+let b = 8
+
+let Soma = a + b
+let Subtracao = a - b
+let Multiplicacao = a * b
+let Divisao = a / b
+
+console.log(a,"+",b,"=",Soma)
+console.log(a,"-",b,"=",Subtracao)
+console.log(a,"*",b,"=",Multiplicacao)
+console.log(a,"/",b,"=",Divisao)
