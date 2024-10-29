c01083c894341a7271a1f9fd7c4c13359eb517c0
caina150909 <cainadpchaves@gmail.com>
2024-10-29 19:44:02 -0300

    calcular a Circunferencia

---
+++ calcular_circunferencia.js
@@ -0,0 +1,10 @@
+// CALCULAR CIRCUFERENCIA
+
+const pi = 3.141592
+
+let raio = 20
+
+let area = pi * (raio ** 2) // Variavel area recebe PI multiplicando raio² 
+
+console.log("O raio é de:", raio)
+console.log("metros quadrados: " +area+ "m2.")
